a56e37ef836f1f3a854a868dc0f17e05548a91bf
GitRocha <jprocha25698@gmail.com>
2021-01-28 20:39:28 +0000

    Issue #8

---
+++ scripts/side_menu.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("burger").style.display = "none";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("burger").style.display = "block";
+  }
